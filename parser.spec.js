fbc5a9ea9e3ac8808d15b8ba149acf516127a42c
Muhammad Faisal Bin Arba'in <faisal.arbain@gmail.com>
2020-01-23 19:49:03 +0800

    failing test for format indicator present

--- parser.spec.js
+++ parser.spec.js
@@ -2,11 +2,19 @@ const expect = require('chai').expect
 
 const parser = require('./parser')
 
+const SAMPLE_1 = '0101I040412347802MY12320010James_Bond01020202108888666699055000007330004Bond0105James0212770707148888'
+const SAMPLE_2 = '0002010101I040412347802MY12320010James_Bond01020202108888666699055000007330004Bond0105James'
+const SAMPLE_3 = '0002010101I040412347802MY12340010James_Bond0102020210888866666699055000007330004Bond0105James0212770707148888'
+
 describe('parser', function () {
   describe('payload format indicator', function () {
     it('throw error when not present as first data', function () {
-      const sample1 = '0101I040412347802MY12320010James_Bond01020202108888666699055000007330004Bond0105James0212770707148888'
-      expect(() => parser.parse(sample1)).to.throw()
+      
+      expect(() => parser.parse(SAMPLE_1)).to.throw()
+    })
+
+    it('have no error when format indicator present', function () {
+      expect(() => parser.parse(SAMPLE_2)).to.not.throw()
     })
   })
 })
