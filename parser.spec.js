d42ada99855fd49cac727117500932dd914ab068
Muhammad Faisal Bin Arba'in <faisal.arbain@gmail.com>
2020-01-23 19:44:34 +0800

    failing test to format indicator required

--- parser.spec.js
+++ parser.spec.js
@@ -1,7 +1,12 @@
-const assert = require('chai').assert
+const expect = require('chai').expect
 
-describe('can run test', function () {
-  it('pass', function () {
-    assert.isTrue(true)
+const parser = require('./parser')
+
+describe('parser', function () {
+  describe('payload format indicator', function () {
+    it('must present as the first data payload', function () {
+      const sample1 = '0101I040412347802MY12320010James_Bond01020202108888666699055000007330004Bond0105James0212770707148888'
+      expect(() => parser.parse(sample1)).to.throw()
+    })
   })
 })
