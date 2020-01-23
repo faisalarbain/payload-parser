ffe08417a0274cce9e170bb3b053f5975357483b
Muhammad Faisal Bin Arba'in <faisal.arbain@gmail.com>
2020-01-23 20:01:31 +0800

    hardcoded parsed ID

---
+++ splitter.js
@@ -0,0 +1,7 @@
+module.exports = {
+  parse(string) {
+    return {
+      ID: '00'
+    }
+  }
+}
--- splitter.spec.js
+++ splitter.spec.js
@@ -1,7 +1,9 @@
 const expect = require('chai').expect
+const splitter = require('./splitter')
 
 describe("splitter", function () {
-  it("can run test", function () {
-    expect(true).equal(true)
+  it('can identify ID', function () {
+    const result = splitter.parse('000201')
+    expect(result.ID).equal('00')
   })
 })
