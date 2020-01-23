feb43f87ff7929e4f8baf26e13eba42b259c95c3
Muhammad Faisal Bin Arba'in <faisal.arbain@gmail.com>
2020-01-23 20:28:41 +0800

    can get content lenght

--- splitter.js
+++ splitter.js
@@ -1,11 +1,16 @@
 const R = require('ramda')
 
+const getContentLength = R.pipe(
+  str => str.substring(2, 4),
+  parseInt
+)
 
 module.exports = {
   parse(string) {
+    const contentLength = getContentLength(string)
     return {
       ID: R.head(R.match(/^[0-9]{2}/, string)),
-      contentLength: 10,
+      contentLength,
     }
   }
 }
