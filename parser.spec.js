ef0091a8369e7c938f97bc478f26a8a1afb89b77
Muhammad Faisal Bin Arba'in <faisal.arbain@gmail.com>
2020-01-23 19:46:21 +0800

    rename test

--- parser.spec.js
+++ parser.spec.js
@@ -4,7 +4,7 @@ const parser = require('./parser')
 
 describe('parser', function () {
   describe('payload format indicator', function () {
-    it('must present as the first data payload', function () {
+    it('throw error when not present as first data', function () {
       const sample1 = '0101I040412347802MY12320010James_Bond01020202108888666699055000007330004Bond0105James0212770707148888'
       expect(() => parser.parse(sample1)).to.throw()
     })
