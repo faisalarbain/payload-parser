9a7b25b3752c6dbf9a9fdcbf3ebe1b4999341862
Muhammad Faisal Bin Arba'in <faisal.arbain@gmail.com>
2020-01-23 20:19:00 +0800

    can get attribute ID

--- splitter.js
+++ splitter.js
@@ -1,7 +1,9 @@
+const R = require('ramda')
+
 module.exports = {
   parse(string) {
     return {
-      ID: '00'
+      ID: R.head(R.match(/^[0-9]{2}/, string))
     }
   }
 }
--- splitter.spec.js
+++ splitter.spec.js
@@ -3,13 +3,15 @@ const splitter = require('./splitter')
 
 describe("splitter", function () {
   const params = [
-    {str: '000201', expected: '00'}
+    {str: '000201', expectedID: '00'},
+    {str: '0101l', expectedID: '01'},
+    {str: '0101M', expectedID: '01'},
   ]
 
   params.forEach(param => {
     it(`can identify ID for [${param.str}]`, function () {
       const result = splitter.parse(param.str)
-      expect(result.ID).equal(param.expected)
+      expect(result.ID).equal(param.expectedID)
     })
   })
   
