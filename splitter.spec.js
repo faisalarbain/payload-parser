bff0d1f2ecd6998ac9752e24f827203721daaf3b
Muhammad Faisal Bin Arba'in <faisal.arbain@gmail.com>
2020-01-23 20:33:26 +0800

    test boundries

--- splitter.spec.js
+++ splitter.spec.js
@@ -3,13 +3,13 @@ const splitter = require('./splitter')
 
 describe("splitter", function () {
   const params = [
-    {str: '000201', expectedID: '00', contentLength: 2, expectedContent: '01'},
-    {str: '0101I', expectedID: '01', contentLength: 1, expectedContent: 'I'},
-    {str: '0101M', expectedID: '01', contentLength: 1, expectedContent: 'M'},
+    {str: '00020112', expectedID: '00', contentLength: 2, expectedContent: '01'},
+    {str: '0101I12', expectedID: '01', contentLength: 1, expectedContent: 'I'},
+    {str: '0101M12', expectedID: '01', contentLength: 1, expectedContent: 'M'},
   ]
 
   params.forEach(param => {
-    it(`can identify ID and content length [${param.str}]`, function () {
+    it(`can identify ID and content [${param.str}]`, function () {
       const result = splitter.parse(param.str)
       expect(result.ID).equal(param.expectedID)
       expect(result.contentLength).equal(param.contentLength)
