76b72efff398a837c8a774a57779f627307558c7
Muhammad Faisal Bin Arba'in <faisal.arbain@gmail.com>
2020-01-24 08:55:19 +0800

    can split all samples

--- second/second.spec.js
+++ second/second.spec.js
@@ -1,6 +1,10 @@
 const parser = require('./parser')()
 const expect = require('chai').expect
 
+const SAMPLE_1 = '0101I040412347802MY12320010James_Bond01020202108888666699055000007330004Bond0105James0212770707148888'
+const SAMPLE_2 = '0002010101I040412347802MY12320010James_Bond01020202108888666699055000007330004Bond0105James'
+const SAMPLE_3 = '0002010101I040412347802MY12340010James_Bond0102020210888866666699055000007330004Bond0105James0212770707148888'
+
 describe.only('second attempt', function () {
   describe('splitter', function () {
     it('return empty array if nothing to split', function () {
@@ -17,6 +21,17 @@ describe.only('second attempt', function () {
 
     it('can split into 2 item', function () {
       expect(parser.split('0002010101M')).to.deep.equal(['000201','0101M'])
+    })
+
+    const allSamples = [
+      { str: SAMPLE_1, expected: 6 },
+      { str: SAMPLE_2, expected: 7 },
+      { str: SAMPLE_3, expected: 7 },
+    ]
+    allSamples.forEach((sample, i) => {
+      it(`can split sample ${i}`, function () {
+        expect(parser.split(sample.str)).has.lengthOf(sample.expected)
+      })
     })
   })
 })
