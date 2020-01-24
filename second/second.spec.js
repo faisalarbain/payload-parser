aecd47a5f93ffe676a25eef37072d7a7639db4db
Muhammad Faisal Bin Arba'in <faisal.arbain@gmail.com>
2020-01-24 08:36:17 +0800

    can handle split empty string

--- second/second.spec.js
+++ second/second.spec.js
@@ -1,7 +1,14 @@
+const parser = require('./parser')
 const expect = require('chai').expect
 
 describe.only('second attempt', function () {
-  it('can run test', function () {
-    expect(true).equal(true)
+  describe('splitter', function () {
+    it('return empty array if nothing to split', function () {
+      expect(parser.split()).to.deep.equal([])
+    })
+
+    it('return empty array if empty string', function () {
+      expect(parser.split('')).to.deep.equal([])
+    })
   })
 })
