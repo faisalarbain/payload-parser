326b04f79b13955d4d4f147affe8ce678b27e89f
Muhammad Faisal Bin Arba'in <faisal.arbain@gmail.com>
2020-01-24 08:44:39 +0800

    works

--- second/parser.js
+++ second/parser.js
@@ -3,6 +3,12 @@ module.exports = {
     if (!str) {
       return output
     }
-    return [...output, str]
+
+    const size = parseInt(str.substring(2, 4))
+    const splitAt = size + 4
+    const item = str.substring(0, splitAt)
+    const remainingStr = str.substring(splitAt)
+
+    return this.split(remainingStr, [...output, item])
   }
 }
